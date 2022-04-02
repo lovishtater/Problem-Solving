c6175c42f5414bcfdd6911be68055101c629fd8b
Lovish Tater <lovishtater@gmail.com>
2022-04-02 11:32:18 +0530

    Time: 18 ms (11.82%), Space: 43.2 MB (82.72%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int j = s.length()-1 ;
+        int i = 0;
+        boolean flag = false;
+        boolean ans1 = true;
+        boolean ans2 = true;
+        while( i<j){
+            if (s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            } else if (s.charAt(i+1) == s.charAt(j)&& !flag){
+                flag = true;
+                i++;
+            } 
+            // else if (s.charAt(i) == s.charAt(j-1) && !flag){
+            //     flag = true;
+            //     j--;
+            // }
+            else{
+                ans1 = false;
+                j--;
+            }
+        }
+        i=0;
+        j = s.length()-1;
+        flag = false;
+                while( i<j){
+            if (s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            } 
+            //         else if (s.charAt(i+1) == s.charAt(j)&& !flag){
+            //     flag = true;
+            //     i++;
+            // } 
+                    else if (s.charAt(i) == s.charAt(j-1) && !flag){
+                flag = true;
+                j--;
+            }
+            else{
+                i++;
+                ans2 = false;
+            }
+        }
+        System.out.println(ans1 + " " + ans2);
+        return (ans1 || ans2);
+    }
+}
