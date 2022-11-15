63c8cf4619f78c4c76ed76bb9a086b05eedd3542
Lovish Tater <lovishtater@gmail.com>
2022-11-16 00:23:39 +0530

    Time: 0 ms (100.00%), Space: 55.1 MB (5.24%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = (start + end) /2;
+            if(target == nums[mid]){
+                return mid;
+            }else if (target< nums[mid]){
+                end = mid-1;
+            }else if (target> nums[mid])
+                start = mid+1;
+            }
+        return -1;
+    }
+}
